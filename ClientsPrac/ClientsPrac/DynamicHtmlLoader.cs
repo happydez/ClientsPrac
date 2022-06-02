24066ae59c9b6363c556cae55562741699e1b430
happydez <mega6ban@mail.ru>
2022-06-03 00:07:39 +0300

    Init project. Added class - DynamicHtmlLoader.

---
+++ ClientsPrac/ClientsPrac/DynamicHtmlLoader.cs
@@ -0,0 +1,47 @@
+﻿using PuppeteerSharp;
+
+namespace dApp.Net
+{
+    public class DynamicHtmlLoader
+    {
+        public string Url { get; init; }
+        public string BrowserPath { get; init; }
+
+        public DynamicHtmlLoader(string url, string browserPath)
+        {
+            if (url is null || browserPath is null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            Url = url;
+            BrowserPath = browserPath;
+        }
+
+        public async Task<string> GetDynamicHtmlAsync()
+        {
+            var options = new LaunchOptions
+            {
+                Headless = true,
+                Product = Product.Chrome,
+                ExecutablePath = BrowserPath
+            };
+
+            var browser = await Puppeteer.LaunchAsync(options, null);
+            var page = await browser.NewPageAsync();
+            var response = await page.GoToAsync(Url);
+
+            if (response.Ok)
+            {
+                return await page.GetContentAsync();
+            }
+            else
+            {
+                return await Task.Run(() =>
+                {
+                    return string.Empty;
+                });
+            }
+        }
+    }
+}
---
+++ ClientsPrac/ClientsPrac/Program.cs
@@ -0,0 +1,18 @@
+﻿using dApp.Net;
+
+namespace dApp
+{
+    public class MainScript
+    {
+        public static readonly string Url = @"https://www.sourcejump.net/leaderboards";
+        public static readonly string Browser = @"C:\Program Files\Google\Chrome\Application\chrome.exe";
+
+        public static async Task Main()
+        {
+            var dynamicHtml = new DynamicHtmlLoader(Url, Browser);
+            var html = await dynamicHtml.GetDynamicHtmlAsync();
+
+            Console.WriteLine(html);
+        }
+    }
+}
